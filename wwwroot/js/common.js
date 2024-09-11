678a9493500c52a5441ed9f8e3cc8627f22f2dee
luv-abhilash123 <abhilashyadav7@gmail.com>
2024-09-11 08:17:27 +0530

    Component CHnages

---
+++ wwwroot/js/common.js
@@ -0,0 +1,31 @@
+﻿window.showToastr = function (type, message) {
+    if (type == "success") {
+    
+        toastr.success(message)
+    }
+    if (type == "error") {
+        toastr.error(message)
+
+
+    }
+    
+}
+
+window.showSweetAlert = function (type, message)
+{
+    if (type == "success") {
+        Swal.fire({
+            title: "Good job!",
+            text: message,
+            icon: "success"
+        });
+    }
+   if(type == "error") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: message,
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+    }
+}
